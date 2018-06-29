182335aced97c3efe6dc22eb7752e22e5d44f504
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2018-06-29 17:15:32 +0200

    Starting the tutorial

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on June 27, 2018, 3:52 pm.
---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AdvertController extends Controller {
+
+    public function indexAction($page){
+        if($page < 1){
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+        }
+
+        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+    }
+
+    public function viewAction($id){
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id
+        ));
+    }
+
+    public function addAction(Request $request){
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    public function editAdction($id, Request $request){
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }
+
+    public function deleteAction($id){
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    }
+}
